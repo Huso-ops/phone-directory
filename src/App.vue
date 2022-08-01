c187b35ad2da8a3e88e9b7e3da9b5729b4f8a9d4
Huso-ops <81871561+Huso-ops@users.noreply.github.com>
2022-08-01 15:54:04 +0300

    pb
    pb

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+
+
+<LookPanel></LookPanel>
+<MyFooter></MyFooter>
+<AddDirectory></AddDirectory>
+
+</template>
+
+<script>
+
+import MyFooter from './components/MyFooter.vue'
+import LookPanel from './components/LookPanel.vue'
+
+export default {
+  name: 'App',
+  components: {
+
+    MyFooter,
+    LookPanel,
+
+}
+}
+</script>
+
+
